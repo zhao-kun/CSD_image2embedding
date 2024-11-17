9c01d5c47e33d1b71ddf873c08c5c4a4238c0266
Kun Zhao <zhaokun.km@gmail.com>
2024-11-17 19:10:15 +0800

    Use fix port instead of random port

--- dash_page.py
+++ dash_page.py
@@ -16,7 +16,7 @@ import webbrowser
 
 def find_free_port():
     while True:
-        port = random.randint(49152, 65535)  # Use dynamic/private port range
+        port = 28080 #random.randint(49152, 65535)  # Use dynamic/private port range
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             try:
                 s.bind(("", port))
