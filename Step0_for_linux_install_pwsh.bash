33b9b3f1eae87616a6b797328dcdf85a06591792
Kun Zhao <zhaokun.km@gmail.com>
2024-11-17 13:55:54 +0800

    remove sudo

--- Step0_for_linux_install_pwsh.bash
+++ Step0_for_linux_install_pwsh.bash
@@ -1,4 +1,5 @@
 #!/usr/bin/bash
+set -e
 #export PIP_INDEX_URL="https://pypi.mirrors.ustc.edu.cn/simple"
 #export HF_ENDPOINT="https://hf-mirror.com"
 
@@ -11,16 +12,16 @@ then
     curl -L -o /tmp/powershell.tar.gz https://github.com/PowerShell/PowerShell/releases/download/v7.4.5/powershell-7.4.5-linux-x64.tar.gz
     
     # 创建目标文件夹
-    sudo mkdir -p /opt/microsoft/powershell/7
+    mkdir -p /opt/microsoft/powershell/7
     
     # 解压 PowerShell 到目标文件夹
-    sudo tar zxf /tmp/powershell.tar.gz -C /opt/microsoft/powershell/7
+    tar zxf /tmp/powershell.tar.gz -C /opt/microsoft/powershell/7 && rm -f /tmp/powershell.tar.gz
     
     # 设置执行权限
-    sudo chmod +x /opt/microsoft/powershell/7/pwsh
+    chmod +x /opt/microsoft/powershell/7/pwsh
     
     # 创建指向 pwsh 的符号链接
-    sudo ln -s /opt/microsoft/powershell/7/pwsh /usr/bin/pwsh
+    ln -s /opt/microsoft/powershell/7/pwsh /usr/bin/pwsh
     
     echo "PowerShell 安装完成"
 else
